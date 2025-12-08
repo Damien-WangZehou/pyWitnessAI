98c3312647e0c34dd61e690f878e21349c1d90f4
Damien Z Wang <752879139h@gmail.com>
2025-12-08 21:40:00 +0000

    fix(__init__): export core classes from package init

--- src/pyWitnessAI/__init__.py
+++ src/pyWitnessAI/__init__.py
@@ -12,4 +12,14 @@ from .LineupDecider import *
 from .VideoLineupPipeline import *
 from .utils import find_bins, plot_hist_with_edges
 
-__all__ = ["find_bins", "plot_hist_with_edges"]
+__all__ = [
+    "plot_hist_with_edges",
+    "Images",
+    "ImagesAI",
+    "Video",
+    "VideoAI",
+    "VideoProcessor",
+    "Lineup",
+    "LineupDecider",
+    "VideoLineupPipeline",
+]
